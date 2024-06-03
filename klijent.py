9c5d292373e7d72f6348eb1b2c0f9a6b173e864e
Milos <milosdjordjevicc19@gmail.com>
2024-06-03 14:40:41 +0200

    Initial commit
    Added client and server with match,add team, add player and close

---
+++ klijent.py
@@ -0,0 +1,185 @@
+import tkinter as tk
+from tkinter import ttk,messagebox
+import json
+import socket
+
+class LetnjaLigaApp:
+    def __init__(self,root):
+        self.root = root
+        self.root.title("Letnja liga - Turnir u malom fudbalu")
+        self.teams = self.load_teams()
+        self.create_main_widgets()
+    def load_teams(self):
+        with open('timovi.json','r') as file:
+            return json.load(file)
+    
+    def create_main_widgets(self):
+        self.match_button = tk.Button(self.root,text="Utakmica",command=self.start_match)
+        self.match_button.pack(pady=10)
+
+        self.add_team_button = tk.Button(self.root,text="Dodaj ekipu", command = self.add_team)
+        self.add_team_button.pack(pady=10)
+
+        self.add_player_button = tk.Button(self.root,text="Dodaj igraca",command = self.add_player)
+        self.add_player_button.pack(pady=10)
+
+        self.results_button = tk.Button(self.root,text="Rezulati",command = self.show_results)
+        self.results_button.pack(pady=10)
+
+        self.exit_button = tk.Button(self.root,text="Izlaz",command = self.root.quit)
+        self.exit_button.pack(pady=10)
+
+    def start_match(self):
+        match_window = tk.Toplevel(self.root)
+        match_window.title("Utakmica")
+        match_window.geometry("300x600")
+        
+        self.team1_label = tk.Label(match_window,text="Tim 1")
+        self.team1_label.pack()
+        
+        self.team1_select = ttk.Combobox(match_window,values=list(self.teams.keys()))
+        self.team1_select.pack()
+        self.team1_select.bind("<<ComboboxSelected>>",lambda _:self.load_players(match_window,self.team1_select,self.team1_players))
+        
+        self.team1_players = tk.Listbox(match_window)
+        self.team1_players.pack()
+
+        self.team2_label = tk.Label(match_window,text="Tim 2")
+        self.team2_label.pack()
+        
+        self.team2_select = ttk.Combobox(match_window,values=list(self.teams.keys()))
+        self.team2_select.pack()
+        self.team2_select.bind("<<ComboboxSelected>>",lambda _:self.load_players(match_window,self.team2_select,self.team2_players))
+        
+        self.team2_players = tk.Listbox(match_window)
+        self.team2_players.pack()
+
+        self.team1_score = 0
+        self.team2_score = 0
+        
+        self.team1_score_label = tk.Label(match_window,text="0")
+        self.team1_score_label.pack()
+
+        self.team2_score_label = tk.Label(match_window,text="0")
+        self.team2_score_label.pack()
+
+        self.goal_team1_button = tk.Button(match_window,text=f"Gol ekipa 1",command = lambda:self.add_goal(self.team1_select,self.team1_players,1))
+        self.goal_team1_button.pack()
+
+        
+        self.goal_team2_button = tk.Button(match_window,text="Gol ekipa 2",command = lambda:self.add_goal(self.team2_select,self.team2_players,2))
+        self.goal_team2_button.pack()
+
+        self.end_match_button = tk.Button(match_window,text="Kraj utakmice",command = lambda: self.end_match(match_window))
+        self.end_match_button.pack()
+
+    def load_players(self,window,select,listbox):
+        team = select.get()
+        listbox.delete(0,tk.END)
+        for player in self.teams[team]:
+            listbox.insert(tk.END,player)
+    def add_goal(self,select,listbox,team):
+        if team == 1:
+            self.team1_score += 1
+            self.team1_score_label.config(text=f"{self.team1_score}")
+        else:
+            self.team2_score +=1
+            self.team2_score_label.config(text=f"{self.team2_score}")
+        selected_player = listbox.get(listbox.curselection())
+        print(f"Igrac {selected_player} je postigao gol za ekipu {team}")
+
+    def end_match(self,match_window):
+        if messagebox.askyesno("Kraj utakmice?","Da li ste sigurni?"):
+            match_data = {
+                "datum" : "2023-06-01",
+                "vreme" : "18:00",
+                "tim1" : self.team1_select.get(),
+                "tim2" : self.team2_select.get(),
+                "rezultat" : f"{self.team1_score} : {self.team2_score}"
+            }
+            self.send_match_data(match_data)
+            match_window.destroy()
+
+    def send_match_data(self,match_data):
+        req = {'action':'add_game','data':match_data}
+        with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+            s.connect(('localhost',8001))
+            s.sendall(json.dumps(req).encode('utf-8'))
+    
+    def save_teams(self):
+        with open('timovi.json','w') as file:
+            json.dump(self.teams,file)
+    
+    def add_team(self):
+        add_team_window = tk.Toplevel(self.root)
+        add_team_window.title("Dodaj ekipu")
+        add_team_window.geometry("300x600")
+        
+        tk.Label(add_team_window,text="Naziv ekipe: ").pack()
+        
+        team_name_entry = tk.Entry(add_team_window)
+        team_name_entry.pack()
+        tk.Button(add_team_window,text="Dodaj",command= lambda : self.save_team(add_team_window,team_name_entry)).pack()
+
+    def save_team(self,add_team_window,team_name_entry):
+        team_name = team_name_entry.get()
+        if team_name and team_name not in self.teams:
+            self.teams[team_name] = []
+            self.save_teams()
+            messagebox.showinfo("Uspeh","Tim uspesno dodat!")
+            add_team_window.destroy()
+        else:
+            messagebox.showerror("Greska","Neispravan naziv ekipe ili ekipa vec postoji")
+    
+        
+    def add_player(self):
+        add_player_window = tk.Toplevel(self.root)
+        add_player_window.title("Dodaj igraca")
+        add_player_window.geometry("300x600")
+        
+        tk.Label(add_player_window,text="Naziv ekipe:").pack()
+        team_select = ttk.Combobox(add_player_window,values = list(self.teams.keys()))
+        team_select.pack()
+        
+        tk.Label(add_player_window,text="Ime i prezime igraca:").pack()
+        player_name_entry=tk.Entry(add_player_window)
+        player_name_entry.pack()
+        tk.Button(add_player_window,text="Dodaj",command=lambda: self.save_player(add_player_window,team_select,player_name_entry)).pack()
+
+
+    def save_player(self,add_player_window,team_select,player_name_entry):
+        team_name = team_select.get()
+        player_name = player_name_entry.get()
+        if team_name and player_name:
+            self.teams[team_name].append(player_name)
+            self.save_teams()
+            messagebox.showinfo("Uspeh","Igrac uspesno dodat")
+        else:
+            messagebox.showerror("Greska","Neispravan naziv ekipe ili ime igraca")
+        
+    def show_results(self):
+        results_window = tk.Toplevel(self.root)
+        results_window.title("Rezultati")
+        results_text = tk.Text(results_window)
+        results_text.pack()
+        req = {'action':'get_games'}
+        with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+            s.connect(('localhost',8001))
+            s.sendall(json.dumps(req).encode('utf-8'))
+            response = s.recv(4096).decode('utf-8')
+            games = json.loads(response)
+            for game in games:
+                results_text.insert(tk.END,f"Datum : {game[1]}, Vreme : {game[2]}, {game[3]} {game[5]} {game[4]}\n")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.geometry("300x400")
+    app = LetnjaLigaApp(root)
+    root.mainloop()
+
+
+        
+        
+
+                
+
---
+++ server.py
@@ -0,0 +1,69 @@
+import socket
+import sqlite3
+import json
+from threading import Thread
+
+class Server:
+        def __init__(self,host='localhost',port=8001):
+            self.host = host
+            self.port = port
+            self.db_path = "liga.db"
+            self.conn = sqlite3.connect('liga.db')
+            self.create_tables()
+        
+        def create_tables(self):
+            cursor = self.conn.cursor()
+            cursor.execute('''
+                CREATE TABLE IF NOT EXISTS utakmice(
+                    id INTEGER PRIMARY KEY,
+                    datum TEXT,
+                    vreme TEXT,
+                    tim1 TEXT,
+                    tim2 TEXT,
+                    rezultat TEXT
+                )
+                '''
+            )
+            self.conn.commit()
+            
+        def handle_client(self,client_socket):
+            with sqlite3.connect(self.db_path) as conn:
+                try:
+                    data = client_socket.recv(1024).decode('utf-8')
+                    req = json.loads(data)
+                    if req['action'] == 'add_game':
+                        self.add_game(conn,req['data'])
+                    elif req['action'] == 'get_games':
+                        games = self.get_games(conn)
+                        client_socket.send(json.dumps(games).encode('utf-8'))
+                except Exception as e:
+                    print(f"Error : {e}")
+                finally:
+                    client_socket.close()
+        
+        def add_game(self,conn,data):
+            cursor = conn.cursor()
+            cursor.execute('INSERT INTO utakmice (datum,vreme,tim1,tim2,rezultat) VALUES (?,?,?,?,?)',(data['datum'],data['vreme'],data['tim1'],data['tim2'],data['rezultat']))
+            conn.commit()
+            print(self.get_games(conn))
+
+        def get_games(self,conn):
+            cursor = conn.cursor()
+            cursor.execute('SELECT * FROM utakmice')
+            return cursor.fetchall()
+
+        def start(self):
+            server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            server_socket.bind((self.host,self.port))
+            server_socket.listen(5)
+            print(f"Server is listening on {self.host} : {self.port}")
+            
+            while True:
+                client_socket,addr = server_socket.accept()
+                print(f"Connection from {addr}")
+                client_handler = Thread(target = self.handle_client,args=(client_socket,))
+                client_handler.start()
+
+if __name__ == "__main__":
+    server = Server()
+    server.start()
